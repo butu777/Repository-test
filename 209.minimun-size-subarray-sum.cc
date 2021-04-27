135cae4b8ff0f0fb36b5dc7bd0067a76294ce18d
butu777 <61738411+butu777@users.noreply.github.com>
2021-04-27 13:28:27 +0800

    Add files via upload
    commit leetcode-209 by dragging files to repository

---
+++ 209.minimun-size-subarray-sum.cc
@@ -0,0 +1,22 @@
+// 暴力求解法
+class Solution {
+public:
+	int minSubArrayLen(int target, vector<int>& nums) {
+		// 暴力解法循环两次
+		int minLen = INT32_MAX;
+		int length = 0;
+		int sum = 0;
+		for(int i = 0; i < nums.size(); ++i) {
+			sum = 0;
+			for(int j = i; j < nums.size(); ++j) {
+				sum += nums[j];
+				if (sum >= target) {
+					length = j - i + 1;
+					minLen = minLen > length ? length : minLen;
+					break;
+				}
+			}
+		}
+	}
+
+};
